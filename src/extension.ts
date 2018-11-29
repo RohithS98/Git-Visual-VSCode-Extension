11688b3f3020a84b4ca97ae6f05511df5b82ffe7
RohithS98 <ee16b025@iittp.ac.in>
2018-11-29 19:18:48 +0530

    Added HTML

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { GitExtension, API, Commit as GitAPI, Repository, Commit } from './git';
+import {VSCExpress} from 'vscode-express';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -12,15 +13,14 @@ const api = gitExtension.getAPI(1);
 
 const rootPath = vscode.workspace.rootPath;
 
-
-async function getCommitDetails(commit : Commit): Promise<string> {
+async function getCommitDetails(commit : Commit): Promise<String> {
     if(commit.parents.length == 0){
-        return commit.message+"\n"+commit.hash+"\n\n"
+        return commit.message + "," + commit.hash;
     }
-    console.log(commit.message, commit.parents[0]);
+    //console.log(commit.message, commit.parents[0]);
     var par = await api.repositories[0].getCommit(commit.parents[0]);
-    var str1 = await getCommitDetails(par);
-    return commit.message+"\n"+commit.hash + "\n\n" + str1;
+    var coms = await getCommitDetails(par);
+    return commit.message + "," + commit.hash + ";" + coms;
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -28,12 +28,17 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "gitvisual" is now active!');
-
+    const vscexpress = new VSCExpress(context, 'view');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
+
+    let disp2 = vscode.commands.registerCommand('extension.vscexpress',() => {
+      });
+
     let disposable = vscode.commands.registerCommand('extension.sayHello', async () => {
         // The code you place here will be executed every time your command is executed
+        vscexpress.open('index.html', 'VS Code Express Example',vscode.ViewColumn.One);
         var headB = api.repositories[0].state.HEAD;
         var comName = headB.commit;
         var com = await api.repositories[0].getCommit(comName);
@@ -41,23 +46,15 @@ export function activate(context: vscode.ExtensionContext) {
         var text = await getCommitDetails(com);
         console.log("Got commit history");
         if(com){
-            var setting: vscode.Uri = vscode.Uri.parse("untitled:" + "C:\githistory.txt");
-            vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
-                vscode.window.showTextDocument(a, 1, false).then(e => {
-                    e.edit(edit => {
-                        edit.insert(new vscode.Position(0, 0), text);
-                    });
-                });
-            }, (error: any) => {
-                console.error(error);
-                debugger;
-            });
+            //console.log(text);
+            console.log(typeof text);
+            return text;
         }
         else{
             console.error("Commit not found. Try again");
+            return "Error";
         }
 
-        console.log(headB)
     });
 
     context.subscriptions.push(disposable);
